419b2dc6d206b25d8925624a7a9c1ba3a3b512a7
uttamnagvadiya <uttamnagvadiya192@gmail.com>
2022-08-15 14:46:00 +0530

    .

---
+++ Web Technology/React JS/myfirst-app/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+    <h1>Hello</h1>
+);
