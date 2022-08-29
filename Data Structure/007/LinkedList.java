b0496d4da9b01f6bccaa58ea28edb885f0b0eace
uttamnagvadiya <uttamnagvadiya192@gmail.com>
2022-08-29 11:11:21 +0530

    .

--- Data Structure/007/LinkedList.java
+++ Data Structure/007/LinkedList.java
@@ -3,16 +3,47 @@ import java.util.Scanner;
 class List{
     Node head;
     public static class Node{
-        int value;
+        double value;
         Node nextAddress;
 
-        public Node(int value){
+        public Node(double value){
             this.value = value;
             nextAddress = null;
         }
     }
 
-   public void insertElement(int value){
+    
+//      For Insert Element
+
+    public void insertElementAtFirst(double value){
+        Node newNode = new Node(value);
+        if (head == null)
+            head = newNode;
+        else{
+            Node New = newNode;
+            New.nextAddress = head;
+            head = New;
+        }
+        System.out.println("Inserted Successfully!");
+    }
+
+    public void insertElementAtSpecificPositiotn(double value, double afterValued) {
+        Node newNode = new Node(value);
+        Node temp = head;
+        if (head == null)
+            head = newNode;
+        else{
+            while(temp.value != afterValued){
+                temp = temp.nextAddress;
+            }
+            newNode.nextAddress = temp.nextAddress;
+            temp.nextAddress = newNode;
+            temp = newNode;
+            System.out.println("Inserted Successfully!");
+        }
+    }
+
+   public void insertElementAtEnd(double value){
         Node newNode = new Node(value);
         if (head == null){
             head = newNode;
@@ -26,18 +57,39 @@ class List{
         }
     }
 
-    // public int deleteElement(){
 
-    // }
+
+//      For Delete Element
+
+    public void deleteElementAtFirst(){
+        if (head == null)
+            System.out.println("Sorry! you can not delete element because list is empty.");
+        else{
+            head = head.nextAddress;
+        }
+    }
+
+    public void deleteSpecificElement(double value) {
+        if (head == null)
+            System.out.println("Sorry! you can not delete element because list is empty.");
+        else{
+            Node temp = head;
+            Node pred = temp;
+            while(temp.value != value){
+                pred = temp;
+                temp = temp.nextAddress;
+            }
+            pred.nextAddress = temp.nextAddress;
+        }
+    }
 
     public void sortLinkedList(Node head) {
         Node temp = head;
-        int result = 0;
+        double result = 0;
         while(temp != null){
             Node newTemp = temp.nextAddress;
             while(newTemp != null){
                 if (temp.value > newTemp.value){
-
                     result = temp.value;
                     temp.value = newTemp.value;
                     newTemp.value = result;
@@ -57,6 +109,8 @@ class List{
         }
     }
 
+    
+
 }
 
 
@@ -70,12 +124,13 @@ public class LinkedList{
             switch (sc.nextInt()) {
                 case 1:
                     System.out.print("Enter the Value : ");
-                    obj.insertElement(sc.nextInt());
+                    obj.insertElementAtEnd(sc.nextDouble());
                     break;
 
                 case 2:
-                    // System.out.println(obj.deleteElement);    
+                    obj.deleteElementAtFirst();    
                     break;
+
 
                 case 3: 
                     obj.displayList();
@@ -84,7 +139,26 @@ public class LinkedList{
                 case 4:
                     obj.sortLinkedList(obj.head);
                     break;
+
+                case 5:
+                    System.out.print("Enter the Value : ");
+                    obj.insertElementAtFirst(sc.nextDouble());
+                    break;
+
+                case 6:
+                    System.out.print("Enter the value you want to enter : ");
+                    double enteredValue = sc.nextDouble();
+                    System.out.print("Enter the value after which you want to insert : ");
+                    double afterValued = sc.nextDouble();
+                    obj.insertElementAtSpecificPositiotn(enteredValue, afterValued);
+                    break;
+
+                case 7:
+                    System.out.print("Enter the value you want to delete : ");
+                    obj.deleteSpecificElement(sc.nextDouble());
                 
+                case 10:
+                    System.exit(0);
                 default:
                     break;
             }
---
+++ Web Technology/React JS/FetchAPI/src/FetchingAPI.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+function FetchingAPI(){
+    const[data, setData] = useState([]);
+    useEffect(()=>{
+        fetch("https://630c3f0f53a833c534263375.mockapi.io/FacultyProject")
+        .then(res => 
+            res.json())
+        .then(res => {
+            setData(res);
+            console.log(data);
+        })
+    });
+
+    const formatedFaculty = data.map((f)=>{
+        return(
+            <div className="card col-md-3" style={{width: "18rem"}}>
+                <img src={f.FacultyImage} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{f.FacultyName}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        );
+    });
+
+    return(<div className="row">
+        {formatedFaculty}
+    </div>);
+}
+
+export default FetchingAPI;
